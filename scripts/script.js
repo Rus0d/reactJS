589d90adf30daba5354fb237386d08cfa97408ae
Rus <zhigit@citrus.com.ua>
2016-07-21 17:04:35 +0300

    new

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,16 @@
 $(function() {
 
+    var HelloWorld = React.createClass({
+        render: function() {
+            return (
+                <h1> Hello world! </h1>
+            );
+        }
+    });
+
+    ReactDOM.render(
+        <HelloWorld />,
+        document.getElementById('content')
+    );
+
 });
