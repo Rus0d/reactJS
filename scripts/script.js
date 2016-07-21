755a4d973bb7f7864ec4b2c09526dd336a965b8b
Rus <zhigit@citrus.com.ua>
2016-07-21 18:00:02 +0300

    new

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,67 @@
 $(function() {
 
-    var HelloWorld = React.createClass({
+    var CONTACTS = [
+        {
+            id: 1,
+            name: 'Darth Vader',
+            phoneNumber: '+250966666666',
+            image: 'http://cs7.pikabu.ru/images/big_size_comm_an/2014-03_7/13962622876915.gif'
+        }, {
+            id: 2,
+            name: 'Princess Leia',
+            phoneNumber: '+250966344466',
+            image: 'http://images6.fanpop.com/image/photos/33100000/CARRIE-FISHER-anakin-vader-and-princess-leia-33186069-190-149.gif'
+        }, {
+            id: 3,
+            name: 'Luke Skywalker',
+            phoneNumber: '+250976654433',
+            image: 'http://www.youshouldshare.me/wp-content/uploads/2015/03/14264215682890-anigif_enhanced-buzz-13518-1367608452-4.gif'
+        }, {
+            id: 4,
+            name: 'Chewbacca',
+            phoneNumber: '+250456784935',
+            image: 'https://media.giphy.com/media/RUUdVZqwpfTRS/giphy.gif'
+        }
+    ];
+
+    var Contact = React.createClass({
+
+       render: function() {
+           return (
+               <li className="contact">
+                   <img className="contact-image" src={this.props.image} width="60px" height="60px" alt=""/>
+                   <div className="contact-info">
+                       <div className="contact-name">{this.props.name}</div>
+                       <div className="contact-number">{this.props.phoneNumber}</div>
+                   </div>
+               </li>
+           );
+       }
+    });
+
+    var ContactsList = React.createClass({
         render: function() {
             return (
-                <h1> Hello world! </h1>
+                <div className="contacts">
+                    <ul className="contacts-list">
+                        {
+                            CONTACTS.map(function(el) {
+                                return <Contact
+                                    key={el.id}
+                                    name={el.name}
+                                    phoneNumber={el.phoneNumber}
+                                    image={el.image}
+                                />;
+                            })
+                        }
+                    </ul>
+                </div>
             );
         }
     });
 
     ReactDOM.render(
-        <HelloWorld />,
+        <ContactsList />,
         document.getElementById('content')
     );
 
